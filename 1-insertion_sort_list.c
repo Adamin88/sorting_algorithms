dd8df191096f53518d5973fae5aecb674a8c4cd7
Adamin88 <elhihi.mohamed@gmail.com>
2023-10-24 18:09:49 -0700

    sorting algo

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,14 +8,14 @@
  */
 int len_list(listint_t *h)
 {
-	int len = 0;
+int len = 0;
 
-	while (h)
-	{
-		len++;
-		h = h->next;
-	}
-	return (len);
+while (h)
+{
+len++;
+h = h->next;
+}
+return (len);
 }
 
 /**
@@ -24,38 +24,38 @@ int len_list(listint_t *h)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *curr = NULL, *one = NULL;
-	listint_t *two = NULL, *three = NULL, *four = NULL;
+listint_t *curr = NULL, *one = NULL;
+listint_t *two = NULL, *three = NULL, *four = NULL;
 
-	if (!list || !(*list) || len_list(*list) < 2)
-		return;
+if (!list || !(*list) || len_list(*list) < 2)
+return;
 
-	curr = *list;
+curr = *list;
 
-	while (curr)
-	{
-		if (curr->prev && curr->n < curr->prev->n)
-		{
-			one = curr->prev->prev;
-			two = curr->prev;
-			three = curr;
-			four = curr->next;
+while (curr)
+{
+if (curr->prev && curr->n < curr->prev->n)
+{
+one = curr->prev->prev;
+two = curr->prev;
+three = curr;
+four = curr->next;
 
-			two->next = four;
-			if (four)
-				four->prev = two;
-			three->next = two;
-			three->prev = one;
-			if (one)
-				one->next = three;
-			else
-				*list = three;
-			two->prev = three;
-			curr = *list;
-			print_list(*list);
-			continue;
-		}
-		else
-			curr = curr->next;
-	}
+two->next = four;
+if (four)
+four->prev = two;
+three->next = two;
+three->prev = one;
+if (one)
+one->next = three;
+else
+*list = three;
+two->prev = three;
+curr = *list;
+print_list(*list);
+continue;
+}
+else
+curr = curr->next;
+}
 }
